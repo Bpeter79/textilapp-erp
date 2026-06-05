a3c5598c18ec29c34fb06ee9f30b143ab99d1a14
Bpeter79 <balazspeter9@gmail.com>
2026-06-05 08:23:10 +0200

    feat: add base layout + supabase + global filters

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardHeader } from '@/components/layout/DashboardHeader'
+import { WidgetGrid } from '@/components/widgets/WidgetGrid'
+
+export default function DashboardPage() {
+  return (
+    <>
+      <DashboardHeader />
+      <WidgetGrid />
+    </>
+  )
+}
